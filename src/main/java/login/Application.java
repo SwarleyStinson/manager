c4a2fce7696018374b15bb722cdb909c92634740
Petr Stepanov <p.stepanov@cardpay.local>
2017-02-09 12:19:33 +1000

    LA-MINOR changes

--- src/main/java/login/Application.java
+++ src/main/java/login/Application.java
@@ -7,7 +7,7 @@ public class Application {
 
     public static void main(String[] args) throws Throwable {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("WEB-INF/config/application-context.xml");
 
     }
 
---
+++ src/main/java/login/Role.java
@@ -0,0 +1,5 @@
+package login;
+
+public enum Role {
+
+}
