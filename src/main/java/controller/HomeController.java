7194689d1e44b1115778fb66bd31662fe664f940
Petr Stepanov <p.stepanov@cardpay.local>
2017-02-09 17:07:45 +1000

    LA-MINOR changes

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,14 +2,13 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServlet;
 
 @Controller
 public class HomeController extends HttpServlet{
 
-    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/home"})
     public String gateway() {
         return "home";
     }
---
+++ src/main/webapp/WEB-INF/views/AllHello.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: p.stepanov
+  Date: 09.02.2017
+  Time: 16:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+<h1> Hello ...</h1>
+<form action="login.jsp" method="post">
+    <input type="submit" value="Sign Out"/>
+</form>
+
+</body>
+</html>
