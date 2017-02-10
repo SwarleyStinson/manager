9bfc37d11b890155ca767782c6fe022f2bc4cea3
Petr Stepanov <p.stepanov@cardpay.local>
2017-02-10 12:53:56 +1000

    LA-MINOR changes

--- src/main/java/controller/WebSecurityConfig.java
+++ src/main/java/controller/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,32 +11,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/home")
-//                .permitAll();
-//
-//        http.authorizeRequests()
-//                .antMatchers("/hello/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
-//                .antMatchers("/hello/admin/**").access("hasRole('ROLE_ADMIN')")
-//                .and().formLogin();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user").password("user").roles("USER");
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/home")
+                .permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/hello/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+                .antMatchers("/hello/admin/**").access("hasRole('ROLE_ADMIN')")
+                .and().formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+    }
 
 }
